acca0327f280205ddbf4a0f79d7bb898d5034ac6
Adam <geekosoft@live.co.uk>
2018-11-02 23:13:00 +0000

    Mostly conforms to Javascript Standard Style now.

--- src/ulList.js
+++ src/ulList.js
@@ -1,50 +1,50 @@
 class ulList {
-  constructor(ulObject, options) {
-    this.ulObject = ulObject;
-    this.valueNames = options.valueNames;
-    this.template = options.template;
-    this.noItemsTemplate = options.noItemsTemplate;
-    this.items = [];
+  constructor (ulObject, options) {
+    this.ulObject = ulObject
+    this.valueNames = options.valueNames
+    this.template = options.template
+    this.noItemsTemplate = options.noItemsTemplate
+    this.items = []
   }
 
-  render(searchString, addedCallback) {
-    //Clear existing
-    this.ulObject.html("");
+  render (searchString, addedCallback) {
+    // Clear existing
+    this.ulObject.html('')
 
-    var added = 0;
+    var added = 0
 
     for (let i of this.items) {
       // Search
-      var shouldAdd = false;
+      var shouldAdd = false
       if (searchString) {
         for (let vN of this.valueNames) {
           if (i[vN].toLowerCase().includes(searchString.toLowerCase())) {
-            shouldAdd = true;
+            shouldAdd = true
           }
         }
       } else {
-        shouldAdd = true;
+        shouldAdd = true
       }
 
       if (!shouldAdd) {
-        continue;
+        continue
       }
 
       // Append to DOM
-      var toAdd = this.template;
+      var toAdd = this.template
       for (let vN of this.valueNames) {
-        toAdd = toAdd.replace(`%${vN}%`, i[vN]);
+        toAdd = toAdd.replace(`%${vN}%`, i[vN])
       }
-      var addedObj = $(toAdd).appendTo(this.ulObject);
-      added++;
+      var addedObj = $(toAdd).appendTo(this.ulObject)
+      added++
 
       if (addedCallback) {
-        addedCallback(i, addedObj);
+        addedCallback(i, addedObj)
       }
     }
 
     if (added === 0 && this.noItemsTemplate) {
-      this.ulObject.html(this.noItemsTemplate);
+      this.ulObject.html(this.noItemsTemplate)
     }
   }
 }
